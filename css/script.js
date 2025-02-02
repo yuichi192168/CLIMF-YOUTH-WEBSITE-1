eed97d3d4a77f2e44494299ddcc565145d6c01ef
MeowTzyッ <149583086+yuichi192168@users.noreply.github.com>
2025-02-02 11:12:56 +0800

    Update script.js

--- css/script.js
+++ css/script.js
@@ -1,3 +1,57 @@
+
+
+$(document).ready(function(){
+
+  $(".ham-burger, .nav ul li a").click(function(){
+   
+    $(".nav").toggleClass("open")
+  
+    $(".ham-burger").toggleClass("active");
+  })      
+  $(".accordian-container").click(function(){
+    $(".accordian-container").children(".body").slideUp();
+    $(".accordian-container").removeClass("active")
+    $(".accordian-container").children(".head").children("span").removeClass("fa-angle-down").addClass("fa-angle-up")
+    $(this).children(".body").slideDown();
+    $(this).addClass("active")
+    $(this).children(".head").children("span").removeClass("fa-angle-up").addClass("fa-angle-down")
+  })
+  
+   $(".nav ul li a, .go-down").click(function(event){
+   if(this.hash !== ""){
+  
+    event.preventDefault();
+  
+    var hash=this.hash; 
+  
+    $('html,body').animate({
+      scrollTop:$(hash).offset().top
+    },800 , function(){
+     window.location.hash=hash;
+    });
+  
+    // add active class in navigation
+    $(".nav ul li a").removeClass("active")
+    $(this).addClass("active")
+   }
+  })
+  })
+
+document.getElementById("generateQuoteBtn1").addEventListener("click", function () {
+  this.classList.add("clicked");
+
+  setTimeout(() => {
+    this.classList.remove("clicked");
+  }, 150);
+});
+
+
+
+
+
+
+
+
 function validateEmail() {
   const emailField = document.getElementById('email');
   const email = emailField.value;
@@ -96,52 +150,6 @@ window.addEventListener('resize', updateSlide);
 
 
 
-
-
-$(document).ready(function(){
-
-  $(".ham-burger, .nav ul li a").click(function(){
-   
-    $(".nav").toggleClass("open")
-  
-    $(".ham-burger").toggleClass("active");
-  })      
-  $(".accordian-container").click(function(){
-    $(".accordian-container").children(".body").slideUp();
-    $(".accordian-container").removeClass("active")
-    $(".accordian-container").children(".head").children("span").removeClass("fa-angle-down").addClass("fa-angle-up")
-    $(this).children(".body").slideDown();
-    $(this).addClass("active")
-    $(this).children(".head").children("span").removeClass("fa-angle-up").addClass("fa-angle-down")
-  })
-  
-   $(".nav ul li a, .go-down").click(function(event){
-   if(this.hash !== ""){
-  
-    event.preventDefault();
-  
-    var hash=this.hash; 
-  
-    $('html,body').animate({
-      scrollTop:$(hash).offset().top
-    },800 , function(){
-     window.location.hash=hash;
-    });
-  
-    // add active class in navigation
-    $(".nav ul li a").removeClass("active")
-    $(this).addClass("active")
-   }
-  })
-  })
-
-document.getElementById("generateQuoteBtn1").addEventListener("click", function () {
-  this.classList.add("clicked");
-
-  setTimeout(() => {
-    this.classList.remove("clicked");
-  }, 150);
-});
 
 
 // Add event listener for scroll
